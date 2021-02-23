5701cd86dcea542a91bfbcf71ed93bc91f874db4
Alex Lowe <loweman64@outlook.com>
2021-02-23 06:13:54 -0500

    feat: Allow the server to parse the request paths

--- app/index.js
+++ app/index.js
@@ -2,10 +2,26 @@
 
 // Dependencies
 const http = require("http");
+const url = require("url");
 
 // The server should respond to all requests with a string
 let server = http.createServer((req, res) => {
+
+  // Base URL
+  let baseURL = "http://" + req.headers.host + "/";
+
+  // Get the URL and parse it
+  let regURL = new URL(req.url, baseURL);
+
+  // Get the path
+  let path = regURL.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send the response
   res.end("Hello, World\n");
+
+  // Log the request path
+  console.log("Request is received on path: " + trimmedPath);
 });
 
 // Start the server, and have it listen on port 3000
