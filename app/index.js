93e32ca64739d8d7f9ca81ec6436377c73b6df55
Alex Lowe <loweman64@outlook.com>
2021-02-23 05:57:46 -0500

    feat: Create basic Nodejs server

---
+++ app/index.js
@@ -0,0 +1,14 @@
+/* Primary for API */
+
+// Dependencies
+const http = require("http");
+
+// The server should respond to all requests with a string
+let server = http.createServer((req, res) => {
+  res.end("Hello, World\n");
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => {
+  console.log("The server is listening on port 3000 now.");
+});
